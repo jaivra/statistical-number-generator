3c4e011517b5f4ec55d35145d86e5b2d0d5bd645
jaivra <jaivra@users.noreply.github.com>
2023-06-06 17:14:20 +0000

    [create-pull-request] automated change

--- dist/index.js
+++ dist/index.js
@@ -426,8 +426,9 @@ try {
 
   const distr = parseDistribution(distribution, DISTRIBUTION_PATTERN);
 
-  if (!distr) { // Pattern not found or no groups matched
-    throw new Error(`${distribution} distribution format is not valid`);
+  // Pattern not found or no groups matched
+  if (!distr) { 
+    throw new Error(`${distribution} distribution format not valid`);
   }
 
   const distributionType = distr['family']
